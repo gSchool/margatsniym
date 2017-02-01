8b0fb6e18708cc2bb4317e9d215caca3f5b147c1
dhouk-Laptop\dhouk <nitrogenatom@gmail.com>
2017-02-01 13:03:52 -0600

    Setting up a bottome nav for going between 2 different screens

--- app/src/main/java/com/houkcorp/margatsniym/activities/ImageListActivity.java
+++ app/src/main/java/com/houkcorp/margatsniym/activities/ImageListActivity.java
@@ -4,26 +4,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-
+import com.houkcorp.margatsniym.R;
 import com.houkcorp.margatsniym.dialogs.InstagramLoginDialog;
+import com.houkcorp.margatsniym.dummy.DummyContent;
 import com.houkcorp.margatsniym.events.LoginEvent;
 import com.houkcorp.margatsniym.fragments.ImageDetailFragment;
-import com.houkcorp.margatsniym.R;
-import com.houkcorp.margatsniym.dummy.DummyContent;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
@@ -98,27 +93,6 @@ public class ImageListActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         EventBus.getDefault().unregister(this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.folowed_list_menu, menu);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.account_menu_item:
-                Intent intent = ImageDetailActivity.newIntent(getBaseContext());
-
-
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Subscribe
---
+++ app/src/main/java/com/houkcorp/margatsniym/activities/NavigationBarActivity.java
@@ -0,0 +1,7 @@
+package com.houkcorp.margatsniym.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class NavigationBarActivity extends AppCompatActivity {
+
+}
