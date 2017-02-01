6049f2d75b21c8e764a000402acca0af6592632c
dhouk-Laptop\dhouk <nitrogenatom@gmail.com>
2017-02-01 17:16:04 -0600

    Tabs are now changing colors properly

--- app/src/main/java/com/houkcorp/margatsniym/activities/NavigationBarActivity.java
+++ app/src/main/java/com/houkcorp/margatsniym/activities/NavigationBarActivity.java
@@ -1,7 +1,20 @@
 package com.houkcorp.margatsniym.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+
+import com.houkcorp.margatsniym.R;
 
 public class NavigationBarActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_navigation_bar);
+
+        BottomNavigationView navigationView = (BottomNavigationView) findViewById(R.id.bottom_nav_bar);
+    }
 }
