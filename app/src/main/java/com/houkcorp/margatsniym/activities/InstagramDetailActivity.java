f72006090b7583ff7b5b72d5103ae4c9e2e1a7c9
dhouk-Laptop\dhouk <nitrogenatom@gmail.com>
2017-02-10 02:08:04 -0600

    Basic setup for the details view

--- app/src/main/java/com/houkcorp/margatsniym/activities/InstagramDetailActivity.java
+++ app/src/main/java/com/houkcorp/margatsniym/activities/InstagramDetailActivity.java
@@ -4,17 +4,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
 import android.view.View;
 
 import com.houkcorp.margatsniym.R;
+import com.houkcorp.margatsniym.fragments.InstagramDetailFragment;
+import com.houkcorp.margatsniym.models.Media;
 
 public class InstagramDetailActivity extends AppCompatActivity {
+    public static final String MEDIA_EXTRA = "MEDIA_EXTRA";
 
-    public static Intent newIntent(Context context) {
-        return new Intent(context, InstagramDetailActivity.class);
+    public static Intent newIntent(Context context, Media media) {
+        Intent intent = new Intent(context, InstagramDetailActivity.class);
+        intent.putExtra(MEDIA_EXTRA, media);
+
+        return intent;
     }
 
     @Override
@@ -27,6 +34,15 @@ public class InstagramDetailActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        Media media = null;
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            media = bundle.getParcelable(MEDIA_EXTRA);
+        }
+
+        InstagramDetailFragment instagramDetailFragment = InstagramDetailFragment.newInstance(media);
+
     }
 
     @Override
--- app/src/main/java/com/houkcorp/margatsniym/fragments/InstagramDetailFragment.java
+++ app/src/main/java/com/houkcorp/margatsniym/fragments/InstagramDetailFragment.java
@@ -1,7 +1,20 @@
 package com.houkcorp.margatsniym.fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+
+import com.houkcorp.margatsniym.activities.InstagramDetailActivity;
+import com.houkcorp.margatsniym.models.Media;
 
 public class InstagramDetailFragment extends Fragment {
 
+    public static InstagramDetailFragment newInstance(Media media) {
+        Bundle extras = new Bundle();
+        extras.putParcelable(InstagramDetailActivity.MEDIA_EXTRA, media);
+
+        InstagramDetailFragment instagramDetailFragment = new InstagramDetailFragment();
+        instagramDetailFragment.setArguments(extras);
+
+        return instagramDetailFragment;
+    }
 }
