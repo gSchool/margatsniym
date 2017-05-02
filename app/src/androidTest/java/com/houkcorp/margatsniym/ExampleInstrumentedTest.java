7e80e38e84e335af203b1f0639bdaeea7b7b1076
Brent Gardner <bgardner@squarelabs.net>
2017-05-02 13:31:22 -0500

    test

--- app/src/androidTest/java/com/houkcorp/margatsniym/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/houkcorp/margatsniym/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.houkcorp.margatsniym", appContext.getPackageName());
+        assertEquals("com.houkcorp.margatsniyma", appContext.getPackageName());
     }
 }
